590874d5b052518ce12528728385ae5d04472b0a
laurahohlsiepe <l.hohlsiepe@gmail.com>
2024-08-14 16:55:53 +0200

    add(): navBar

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,13 @@
+import NavBar from "@/scenes/navbar"
+import { useState } from "react";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+
   return (
-   <div className="app">
-      app 
+   <div className="app bg-gray-20">
+      <NavBar selectedPage={ selectedPage }
+              setSelectedPage={ setSelectedPage } />
    </div>
   )
 }
