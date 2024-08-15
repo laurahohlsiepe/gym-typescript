1c944bd187ac6176f86220133d7763a030451820
laurahohlsiepe <l.hohlsiepe@gmail.com>
2024-08-15 16:29:27 +0200

    add(): navBar

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,29 @@
 import NavBar from "@/scenes/navbar"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types"
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
    <div className="app bg-gray-20">
-      <NavBar selectedPage={ selectedPage }
-              setSelectedPage={ setSelectedPage } />
+      <NavBar 
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage} 
+              isTopOfPage={isTopOfPage} />
    </div>
   )
 }
