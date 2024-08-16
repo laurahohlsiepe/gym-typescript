61f9fc2c74321791a306afc9a06e27d6c316235d
laurahohlsiepe <l.hohlsiepe@gmail.com>
2024-08-16 19:05:25 +0200

    add(): motion.div for home

--- vite-project/src/scenes/home/index.tsx
+++ vite-project/src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -25,7 +26,16 @@ const Home = ({ setSelectedPage }: Props) => {
            { /* Main Header */ } 
            <div className="z-10 mt-32 md:basis-3/5" >
             { /* Headings */ }
-            <div className="md:-mt-20">
+            <motion.div className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{ 
+                hidden: { opacity: 0, x: -100},
+                visible: { opacity: 1, x: 0}
+            }}
+            >
              <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img alt="home-page-text" src={HomePageText} />
@@ -37,7 +47,7 @@ const Home = ({ setSelectedPage }: Props) => {
                    Studios to get the Body Shapes That you Dream of.. Get Your Dream
                     Body Now.
                 </p>
-           </div>
+           </motion.div>
 
            { /*Actions*/ }
            <div className="mt-8 flex items-center gap-8">
