80e76a4de56f4ae2139387d4a35d53e15e9e9741
laurahohlsiepe <l.hohlsiepe@gmail.com>
2024-08-18 13:53:15 +0200

    fix(): style

--- vite-project/src/scenes/ourclasses/Class.tsx
+++ vite-project/src/scenes/ourclasses/Class.tsx
@@ -14,8 +14,8 @@ text-white opacity-0 transition duration-500 hover:opacity-90`
   return (
     <li className="relative mx-5 inline-block h-[380px] w-[450px]">
         <div className={overlayStyles} >
-            <p>{name}</p>
-            <p>{description}</p>
+            <p className="text-2xl">{name}</p>
+            <p className="mt-5">{description}</p>
         </div>
         <img alt={`${image}`} src={image} />
     </li>
